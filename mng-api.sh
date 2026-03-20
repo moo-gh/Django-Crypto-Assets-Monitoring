4d6cf551ae5a220bac0e6ea9ab608f3013e7128f
Mo <mo.ghorb@gmail.com>
2026-03-20 10:52:19 -0700

    chore(mng-api): set script directory and change working directory

--- mng-api.sh
+++ mng-api.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
 PROJECT_NAME='crypto_assets'
 SERVER_NAME='DigitalOcean'
 SERVER_PATH='/var/www/crypto_assets/'
